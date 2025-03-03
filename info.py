a742694e6d5b80dff77135fd3c798f90cbbe9f51
Mrhacklx <142010590+Mrhacklx@users.noreply.github.com>
2025-03-03 17:20:26 +0530

    Update info.py

--- info.py
+++ info.py
@@ -122,8 +122,8 @@ VERIFY_SND_SHORTLINK_API = environ.get('VERIFY_SND_SHORTLINK_API', '')
 
 # Shortlink Info
 SHORTLINK_MODE = bool(environ.get('SHORTLINK_MODE', True)) # Set True Or False
-SHORTLINK_URL = environ.get('SHORTLINK_URL', 'crlinks.in')
-SHORTLINK_API = environ.get('SHORTLINK_API', '72993f9f1c919df98b21697db2d9c73896fd85c1')
+SHORTLINK_URL = environ.get('SHORTLINK_URL', 'arlinks.in')
+SHORTLINK_API = environ.get('SHORTLINK_API', '2b9ca12568f523343b5fe52565166d4cd98a4702')
 TUTORIAL = environ.get('TUTORIAL', 'https://t.me/How_to_Open0') # How Open Shortner Link Video Link , Channel Link Where You Upload Your Video.
 
 
