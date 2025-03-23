fa56235efa34960ecba3e21e653f416b8e8cfda2
Mrhacklx <142010590+Mrhacklx@users.noreply.github.com>
2025-03-23 23:07:39 +0530

    Update info.py

--- info.py
+++ info.py
@@ -87,7 +87,7 @@ PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Us
 GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+_fX0T3Ivz9ZiZTI1')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/+QeTfo1zk9WFjMjFl')
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 't.me/+QeTfo1zk9WFjMjFl') # Support Chat Link Without https:// or @
-OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/Mr_attherate')
+OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/+QeTfo1zk9WFjMjFl')
 
 # True Or False
 AI_SPELL_CHECK = bool(environ.get('AI_SPELL_CHECK', True))
